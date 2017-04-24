e99b43b2035a1a17fd29c4dbaf80ded110248bb9
Matheus193dn <matheus@Minhs-MacBook-Pro.local>
2017-04-24 22:36:50 +0700

    Drawing_Demo
    Aa

---
+++ DrawingDemo/DrawingDemo/ViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  DrawingDemo
+//
+//  Created by Minh Hoang Nguyen on 04/22/17.
+//  Copyright © 2017 Minh Hoang Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var swiped = false
+    var lastPoint = CGPoint.zero
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    //lay vi tri
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        swiped = false
+        
+        if let touch = touches.first {
+            lastPoint = touch.location(in: self.view)
+        }
+        
+    }
+    
+    //ham ve
+    func drawLines(fromPoint: CGPoint, toPoint: CGPoint) {
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        imageView.image?.draw(in: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height))
+        
+        let content = UIGraphicsGetCurrentContext()
+        
+        content?.move(to: CGPoint(x: fromPoint.x, y: fromPoint.y))
+        content?.addLine(to: CGPoint(x: toPoint.x, y:toPoint.y))
+        
+        content?.setBlendMode(CGBlendMode.normal)
+        content?.setLineCap(CGLineCap.round)
+        content?.setLineWidth(5)
+        content?.setStrokeColor(UIColor(red: 0, green: 0, blue: 0, alpha: 1.0).cgColor)
+        
+        content?.strokePath()
+        imageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
+
+    //ve khi keo
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        swiped = true
+        if let touch = touches.first {
+            let currentPoint = touch.location(in: self.view)
+            drawLines(fromPoint: lastPoint, toPoint: currentPoint)
+            
+            lastPoint = currentPoint
+        }
+    }
+    
+    //ve khi cham 1 lan
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if !swiped {
+            drawLines(fromPoint: lastPoint, toPoint: lastPoint)
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
